2c444523a6efc593ac24d76cedce02c3cde48c01
MeCodeMyself <spearmanskyla@gmail.com>
2020-09-26 17:44:57 -0500

    Finished Ball physics and first opponent. completely.
    Things are looking pretty good, the game is playable in this state, as there is a functional opponent and ball. Not like the game is done though.

--- GodotPong/Ball.gd
+++ GodotPong/Ball.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = 600
+var speed = 550
 var velocity = Vector2.ZERO
 
 func _ready():
@@ -9,4 +9,6 @@ func _ready():
 	velocity.y = [-0.8,0.8][randi() % 2]
 
 func _physics_process(delta):
-	move_and_slide(velocity * speed)
+	var collision_object = move_and_collide(velocity * speed * delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.normal)
