b8da3709c1806bd67059e2d6b539fd4e06c2ddfd
MeCodeMyself <spearmanskyla@gmail.com>
2020-09-27 23:14:02 -0500

    Updated some code and stuff, started countdown...having problems.
    I stopped development after I got a little too confused on small bugs, like improper collision and stuff that should be easy to fix. I'm really close to finishing the course however. Even though it was originally supposed to be 2 hours...

--- GodotPong/World.gd
+++ GodotPong/World.gd
@@ -4,14 +4,24 @@ var PlayerScore = 0
 var OpponentScore = 0
 
 
-func _on_Left_body_entered(body):
-	$Ball.position = Vector2(312,512)
-	PlayerScore += 1
+func _on_Left_body_entered(_body):
+	score_achieved()
 
-func _on_Right_body_entered(body):
-	$Ball.position = Vector2(312,512)
-	OpponentScore += 1
+func _on_Right_body_entered(_body):
+	score_achieved()
 
-func _process(delta):
+func _process(_delta):
 	$PlayerScore.text = str(PlayerScore)
 	$OpponentScore.text = str(OpponentScore)
+	$CountdownLabel.text = str(int($CountdownTimer.time_left)+1)
+
+func _on_CountdownTimer_timeout():
+	get_tree().call_group('BallGroup','restart_ball')
+	$CountdownTimer.visible = false
+
+func score_achieved():
+	$Ball.position = Vector2(512,304)
+	OpponentScore += 1
+	get_tree().call_group('BallGroup','stop_ball')
+	$CountdownTimer.start()
+	$CountdownTimer.visible = true
